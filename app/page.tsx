33c82a27e17206fa7bf1f4f41c8469de15f1a32a
prasenjeet066 <prasenjeethowlader006@gmail.com>
2025-08-31 18:13:45 +0600

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 // app/page.tsx (Next.js 13+ with App Router)
 "use client";
 import Header from "@/components/header"
+import Content from "@/components/content"
 import { motion } from "framer-motion";
 import { Github, Linkedin, Mail } from "lucide-react";
 import Image from "next/image";
@@ -9,7 +10,7 @@ export default function ProfilePage() {
   return (
     <main className="min-h-screen bg-gray-50 flex items-center  p-6">
       <Header/>
-      
+      <Content/>
     </main>
   );
 }
