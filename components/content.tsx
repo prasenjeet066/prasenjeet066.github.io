210f53669763f926bacc738d80d254970d1d01da
prasenjeet066 <prasenjeethowlader006@gmail.com>
2025-08-31 19:28:10 +0600

    update

--- components/content.tsx
+++ components/content.tsx
@@ -1,7 +1,18 @@
+import TextType from '@/components/text-anim';
 export default function Content() {
   return (
-    <div>
-      
+    <div className='w-full p-4'>
+      <div className ='rounded-md bg-white p-4'>
+        
+
+<TextType 
+  text={["First, Thanks for coming!", "for your websites", "Happy coding!"]}
+  typingSpeed={75}
+  pauseDuration={1500}
+  showCursor={true}
+  cursorCharacter="|"
+/>
+      </div>
     </div>
   )
 }
