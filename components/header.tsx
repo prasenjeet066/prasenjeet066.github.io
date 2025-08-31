b6b1a9db11d7cd321795530faaa279aab5a5899d
prasenjeet066 <prasenjeethowlader006@gmail.com>
2025-08-31 18:16:17 +0600

    update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -14,14 +14,14 @@ export default function Header() {
     },
   ]
   return (
-    <div className='flex p-4 flex-row items-center justify-between'>
+    <div className='flex p-4 flex-row items-center justify-between w-full'>
       <span className ='text-md font-bold'>
         {"Prasenjeet"}
       </span>
-      <div className='flex flex-row items-center'>
+      <div className='flex flex-row items-center flex-1'>
         {
           listNavs.map((nav)=>(
-            <a className='text-sm text-gray-800 underline-none hover:underline' href = {nav.name.split(' ')[0].toLocaleLowerCase()}>
+            <a className='text-sm text-gray-800 px-4 underline-none hover:underline' href = {nav.name.split(' ')[0].toLocaleLowerCase()}>
               {nav.name}
             </a>
           ))
