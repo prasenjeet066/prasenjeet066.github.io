e91a42b312cb608e0d0b733b113069a5cd5fa444
prasenjeet066 <prasenjeethowlader006@gmail.com>
2025-08-31 21:47:59 +0600

    update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -17,9 +17,9 @@ export default function Header() {
         initial={{ opacity: 0, x: -20 }}
         animate={{ opacity: 1, x: 0 }}
         transition={{ duration: 0.6, delay: 0.2 }}
-        className="text-lg logo tracking-wide"
+        className="text-lg logo tracking-wide text-red-600"
       >
-        Prasenjeet
+        PrasenJeet
       </motion.span>
 
       {/* Navigation + LinkedIn button */}
