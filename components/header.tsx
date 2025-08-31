c0dd438765e071c2b3a178a16f6c128a6b723d25
prasenjeet066 <prasenjeethowlader006@gmail.com>
2025-08-31 18:11:07 +0600

    update

---
+++ components/header.tsx
@@ -0,0 +1,32 @@
+export default function Header() {
+  const listNavs = [
+    {
+      name : 'Home'
+    },
+    {
+      name : 'About Me'
+    },
+    {
+      name : 'Projects'
+    },
+    {
+      name : 'Hire Me'
+    },
+  ]
+  return (
+    <div className='flex p-4 flex-row items-center justify-between'>
+      <span className ='text-md font-bold'>
+        {"Prasenjeet"}
+      </span>
+      <div className='flex flex-row items-center'>
+        {
+          listNavs.map((nav)=>(
+            <a className='text-sm text-gray-800 underline-none hover:underline' href = {nav.name.split(' ')[0].toLocaleLowerCase()}>
+              {nav.name}
+            </a>
+          ))
+        }
+      </div>
+    </div>
+  )
+}
