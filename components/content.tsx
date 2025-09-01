562c47c88056ee74989e0d047e93fe7b3f6b5563
prasenjeet066 <prasenjeethowlader006@gmail.com>
2025-09-01 12:27:13 +0600

    update content.tsx

--- components/content.tsx
+++ components/content.tsx
@@ -92,10 +92,23 @@ export default function Content() {
       
       {/* Text Section with gradient background */}
       <div className="flex-1 w-full flex flex-col items-start gap-4 p-6 rounded-xl">
-        <p>
-          
-          <HashtagHighlighter text={details}/>
-        </p>
+        
+<figure class="max-w-screen-md mx-auto text-center">
+    <svg class="w-10 h-10 mx-auto mb-3 text-gray-400 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 14">
+        <path d="M6 0H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3H2a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Zm10 0h-4a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3h-1a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Z"/>
+    </svg>
+    <blockquote>
+        <p class="text-2xl italic font-medium text-gray-900 dark:text-white">{details.slice(0,250)}</p>
+    </blockquote>
+    <figcaption class="flex items-center justify-center mt-6 space-x-3 rtl:space-x-reverse">
+        <img class="w-6 h-6 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/michael-gouch.png" alt="profile picture"/>
+        <div class="flex items-center divide-x-2 rtl:divide-x-reverse divide-gray-500 dark:divide-gray-700">
+            <cite class="pe-3 font-medium text-gray-900 dark:text-white">Michael Gough</cite>
+            <cite class="ps-3 text-sm text-gray-500 dark:text-gray-400">CEO at Ry+</cite>
+        </div>
+    </figcaption>
+</figure>
+
         <TextType
           text={[
             "Python",
@@ -130,7 +143,7 @@ export default function Content() {
       </div>
 
       {/* Image Section */}
-      <div className="flex-1 relative w-full h-[400px] shadow-[15px_16px_0px_-6px_rgba(0,_0,_0,_1)]">
+      <div className="flex-1 relative w-full h-[400px] shadow-[6px_6px_0px_-3px_rgba(0,_0,_0,_1)]">
         <Image
           src="/img/pf.png"
           alt="Profile picture of Prasenjeet Howlader"
