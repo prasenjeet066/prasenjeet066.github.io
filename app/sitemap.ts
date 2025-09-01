63c782263e616b3d053527d34712a7e03cd59672
prasenjeet066 <prasenjeethowlader006@gmail.com>
2025-09-01 13:37:10 +0600

    update

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,16 +1,30 @@
 import { MetadataRoute } from "next";
 //import { getAllPosts } from "./lib/posts"; // your data fetching logic
 
-export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+export default async function sitemap(): Promise < MetadataRoute.Sitemap > {
   const baseUrl = "https://prasenjeet066.github.io";
   //const posts = await getAllPosts();
-
+  
   /**const postUrls = posts.map(post => ({
     url: `${baseUrl}/posts/${post.slug}`,
     lastModified: new Date(post.updatedAt),
   }));**/
-
+  
   return [
     { url: baseUrl, lastModified: new Date() },
+    {
+      url: baseUrl+'/hire',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      
+      url: baseUrl+'/project',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    
+    }
   ];
 }
